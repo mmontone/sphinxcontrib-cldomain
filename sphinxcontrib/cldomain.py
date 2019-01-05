fd2e4d83d70a0b817aa37c04b032bf8ca8f437ee
Alexander Artemenko <svetlyak.40wt@gmail.com>
2019-01-06 00:20:40 +0300

    Fixed typo.

--- sphinxcontrib/cldomain.py
+++ sphinxcontrib/cldomain.py
@@ -898,7 +898,7 @@ def index_packages(systems, system_paths, packages, quicklisp, lisps, cl_debug):
             pprint.pprint(lisp_data)
     except:
         if os.environ.get('CI'):
-            print('A error occurred with the json output from cldomain.\nHere is the full output:\n\n', file=sys.stderr)w
+            print('A error occurred with the json output from cldomain.\nHere is the full output:\n\n', file=sys.stderr)
             print(raw_output.encode('utf-8'), file=sys.stderr)
             print('\nEnd of cldomain output', file=sys.stderr)
         else:
